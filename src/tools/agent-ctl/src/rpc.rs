13f680aa59d7f5f3adddbed6c4e26f82df8932f2
Sumedh Alok Sharma <sumsharma@microsoft.com>
2025-02-04 13:00:58 +0530

    agent-ctl: Second draft to add test vm boot support with cloud hypervisor
    - Move vm boot/shutdown before constructing client
    - Register hypervisor config plugin for loading from config file

--- src/tools/agent-ctl/src/rpc.rs
+++ src/tools/agent-ctl/src/rpc.rs
@@ -10,10 +10,26 @@ use slog::{o, Logger};
 
 use crate::client::client;
 use crate::types::Config;
+use crate::vm;
+use slog::debug;
 
 pub fn run(logger: &Logger, cfg: &Config, commands: Vec<&str>) -> Result<()> {
     // Maintain the global logger for the duration of the ttRPC comms
     let _guard = slog_scope::set_global_logger(logger.new(o!("subsystem" => "rpc")));
 
-    client(cfg, commands)
+    // Booting a test pod vm
+    let test_vm_instance = vm::boot_test_vm()?;
+    debug!(sl!(), "test vm booted for hypervisor: {:?}", test_vm_instance.hypervisor_name);
+
+    match client(cfg, commands) {
+        Ok(_) => {
+		debug!(sl!(), "Shutting down vm");
+		vm::stop_test_vm(test_vm_instance.hypervisor_instance.clone())
+	}
+        Err(e) => {
+		debug!(sl!(), "Command failed: {}", e);
+		debug!(sl!(), "Shutting down vm");
+		vm::stop_test_vm(test_vm_instance.hypervisor_instance.clone())
+	}
+    }
 }
